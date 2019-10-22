6f292bf3844076b8d0aa746f5ca340be97fb3e51
Tilman Raendchen <TRaendchen@outlook.com>
2019-10-22 22:34:18 +0100

    Implement a basic handler manager

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 require("dotenv").config();
-
 global.join = require("path").join;
+
 const client = require(join(process.cwd(), "_client"));
 
 if (!process.env.TOKEN)
     throw new Error("Bot TOKEN is missing in env_vars.");
 
 client.login(process.env.TOKEN)
-    .then(function()
-    {
-        console.log(`Logged in and ready to serve ${client.guilds.size} server${(client.guilds.size === 1) ? "" : "s"}.`);
-    })
     .catch(console.error);
+
+client.once("ready", function()
+{
+    console.log(`Logged in and ready to serve ${client.guilds.size} server${(client.guilds.size === 1) ? "" : "s"}.`);
+
+    client.utils = require(join(process.cwd(), "lib", "utils"));
+    client.handlers = require(join(process.cwd(), "lib", "handlers"));
+    client.commands = require(join(process.cwd(), "lib", "commands"));
+
+    const msgHandler = new client.handlers.message();
+    this.on("message", msgHandler.handle.bind(msgHandler));
+});
